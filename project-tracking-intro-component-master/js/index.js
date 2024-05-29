5ea32e4be07f3de67255ba86391e7fb5cd780017
MUHAMMAD UBAID BIN RASHID <ubaidqadri97@gmail.com>
2024-05-29 08:34:15 -0700

    ok

---
+++ project-tracking-intro-component-master/js/index.js
@@ -0,0 +1,28 @@
+// function borderHandler(){
+//     let login = document.querySelector('.login')
+
+//     login.classList.add('hover')
+  
+// // alert('hello')
+// }
+// let classic = document.querySelector('.Login');
+
+
+// function borderHandler() {
+//     // classic.classList.add('hover');
+//     classic.classList.add('hover');
+    
+// }
+function nav(){
+    // document.querySelector('ul').classList.add('transition')
+    // document.querySelector('ul').classList.remove('transition-two')
+    document.querySelector('.second-nav').classList.remove('for-none')
+    document.querySelector('.menu').classList.add('for-none')
+    document.querySelector('.close').classList.remove('for-none')
+}
+function navClose(){
+
+    document.querySelector('.second-nav').classList.add('for-none')
+    document.querySelector('.menu').classList.remove('for-none')
+    document.querySelector('.close').classList.add('for-none')
+}
